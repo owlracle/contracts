c42b7d3d0db82f8b58bb07f9512dda7e7890875f
werlang <pswerlang@gmail.com>
2022-10-04 18:23:08 +0000

    Clean some thing to make public..

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 async function main() {
-    const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-    const Contract = await hre.ethers.getContractFactory("Lock");
-    const contract = await Contract.attach(contractAddress);
-    
-    const res = await contract.withdraw();
-
-    console.log("Response:", res);
+    // there will be something here.
 }
 
 main()
